f95fd6b444c4b8c965442a4b0138fdbedd1a12bd
Matt Payne <Payne@MattPayne.org>
2019-02-02 18:57:38 -0600

    node index.js # works

--- index.js
+++ index.js
@@ -1,5 +1,36 @@
-var xmltv = require('xmltv');
+var fs = require('fs');
+var path = require('path');
+var moment = require('moment');
+
+let xmltv = require('xmltv');
+
+
+function createParser (xmlName, programmeArray) {
+    var input = fs.createReadStream(path.join(__dirname, xmlName));
+    var parser = new xmltv.Parser();
+    input.pipe(parser);
+
+    parser.on('programme', function (programme) {
+        programmeArray.push(programme);
+    });
+
+    return parser;
+}
+
+
+var guideProgrammes = [];
+var guideParser = createParser('tvguide.xml', guideProgrammes);
+guideParser.on('error', function (err) {
+	console.log(`ERROR!!  ${err}`);
+});
+guideParser.on("end", function() {
+	console.log(guideProgrammes);
+});
+
+/*
 xmltv.on("programme", function(programme) {
   //Do something with programmes one by one as they are parsed
+  console.log(programme);
 });
 xmltv.parseFile("tvguide.xml");
+*/
