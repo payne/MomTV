8b25a3ed0e534874c49d2909dde6660c2b5f6988
Matt Payne <Payne@MattPayne.org>
2019-02-02 20:56:35 -0600

    prettier index.js  > x.js;mv x.js index.js

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
-let fs = require('fs');
-let path = require('path');
-let moment = require('moment');
+let fs = require("fs");
+let path = require("path");
+let moment = require("moment");
 
-let xmltv = require('xmltv');
-let xmlName='tvguide.xml';
+let xmltv = require("xmltv");
+let xmlName = "tvguide.xml";
 let guideProgrammes = [];
 
-    let input = fs.createReadStream(path.join(__dirname, xmlName));
-    let parser = new xmltv.Parser();
-    input.pipe(parser);
+let input = fs.createReadStream(path.join(__dirname, xmlName));
+let parser = new xmltv.Parser();
+input.pipe(parser);
 
-    parser.on('programme', function (programme) {
-      guideProgrammes.push(programme);
-    });
+parser.on("programme", function(programme) {
+  guideProgrammes.push(programme);
+});
 
-
-
-parser.on('error', function (err) {
-	console.log(`ERROR!!  ${err}`);
+parser.on("error", function(err) {
+  console.log(`ERROR!!  ${err}`);
 });
 parser.on("end", function() {
-	console.log(guideProgrammes);
+  console.log(guideProgrammes);
 });
-
