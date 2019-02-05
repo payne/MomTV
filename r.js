fc82359a27d3fbe1b53cd4fc900e784808f74426
Matt Payne <Payne@MattPayne.org>
2019-02-04 18:16:30 -0600

    Parsing times with moment.js.

--- r.js
+++ r.js
@@ -1,17 +1,19 @@
-'use strict';
+"use strict";
 
-const fs = require('fs');
+const fs = require("fs");
+const moment  = require("moment");
 
-let rawdata = fs.readFileSync('tvguide.json');  
-let student = JSON.parse(rawdata);  
+let rawdata = fs.readFileSync("tvguide.json");
+let student = JSON.parse(rawdata);
 //let idArray = student.tv.channel.map(c => c.id);
-let id2name = student.tv.channel.reduce((m,o) => {
-  m[o.id]=o['display-name'];
+let id2name = student.tv.channel.reduce((m, o) => {
+  m[o.id] = o["display-name"];
   return m;
 }, {});
 // console.log(JSON.stringify(id2name));
 student.tv.programme.forEach(p => {
-				let cNumber=id2name[p.channel][2];
-				console.log(`${cNumber}\t${p.title.$t}`);
+  let cNumber = id2name[p.channel][2];
+  let a = moment(p.start, 'YYYYMMDDhhmmss');
+  let b = moment(p.stop,  'YYYYMMDDhhmmss');
+  console.log(`${cNumber}\t${a} - ${b}: ${p.title.$t}`);
 });
-
